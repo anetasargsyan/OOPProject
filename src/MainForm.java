aa7944ccb04beba67ce173ca6b7f0d27f0ae7de1
anetsargsyan <113536009+anetasargsyan@users.noreply.github.com>
2023-05-20 12:35:01 +0400

    Update on MainForm

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame {
     private JButton remove;
@@ -21,17 +25,30 @@ public class MainForm extends JFrame {
         setSize(new Dimension(500, 500));
         setContentPane(mainPanel);
         setVisible(true);
+
+        remove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                list.remove(list.getSelectedIndex());
+            }
+        });
+
         list.setModel(insectsModel);
+        list.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent listSelectionEvent) {
+                if(list.getSelectedIndex() == -1){
+                    remove.setEnabled(false);
+                }
+                else {
+                    remove.setEnabled(true);
+                }
+            }
+        });
         insectsModel.addElement(new Bee("fly", "someone", 4, 4, true, "No facts"));
         insectsModel.addElement(new Ant("walk", "someone", 4, 4, true, "No facts"));
         insectsModel.addElement(new Bee("fly", "someone", 4, 4, true, "No facts"));
     }
 
-    private void add(){
 
-    }
-
-    private void remove(Insect i){
-
-    }
 }
