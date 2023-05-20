858dbaa840d8300461259323b10b1f9a4a3363cb
anetsargsyan <113536009+anetasargsyan@users.noreply.github.com>
2023-05-20 12:17:53 +0400

    Edited Main Form

--- src/Encyclopedia.java
+++ src/Encyclopedia.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class Encyclopedia {
     // Make sure to use your own filepath for the CSVReader below
-    CSVReader reader = new CSVReader("C:\\Users\\kevom\\Documents\\GitHub\\OOPProject\\src\\Data.csv");
+   // CSVReader reader = new CSVReader("C:\\Users\\kevom\\Documents\\GitHub\\OOPProject\\src\\Data.csv");
 
     Insect[] insects = new Insect[]{};
 
@@ -14,12 +14,20 @@ public class Encyclopedia {
         this.insects = insects;
     }
 
-    public static void main(String[] args) throws IvalidCSVFileException, IOException {
-        CSVReader reader = new CSVReader("C:\\Users\\kevom\\Documents\\GitHub\\OOPProject\\src\\Data.csv");
-        System.out.println(Arrays.toString(reader.getColumnNames()));
-        while (reader.hasNextRow()) {
-            String[] row = reader.nextRow();
-            System.out.println(Arrays.toString(row));
-        }
+    Encyclopedia() throws IvalidCSVFileException, IOException {
+
+        //CSVReader reader = new CSVReader("Data.csv");
+
+        //System.out.println(Arrays.toString(reader.getColumnNames()));
+        //while (reader.hasNextRow()) {
+        //    String[] row = reader.nextRow();
+        //    System.out.println(Arrays.toString(row));
+
+        Ant a1 = new Ant("Fire", "L", 4, 4, true, "Something");
+        Ant a2 = new Ant("F2", "L3", 421, 4, false, "Something else");
+        Ant a3 = new Ant("non fire", "L5", 4, 4, true, "Something interesting");
+        insects = new Insect[]{
+                a1,a2,a3
+        };
     }
 }
